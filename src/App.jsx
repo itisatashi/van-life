503a16d4200982c838bb22c8faf00141c4da3a7b
Atabek <superatabekt@gmail.com>
2025-04-02 22:04:51 +0500

    Updated App.jsx and index.css, added About page and banner image

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 export default function App() {
   return (
     <BrowserRouter>
       <header>
         <nav>
-          <Link className="logo">#Vanlife</Link>
+          <Link className="logo" to="/">
+            #Vanlife
+          </Link>
           <ul className="nav-list">
             <li className="nav-item">
-              <Link className="nav-link">About</Link>
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link">Vans</Link>
@@ -20,6 +25,7 @@ export default function App() {
       </header>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
       </Routes>
       <footer>
         <p>Ⓒ 2022 #VANLIFE</p>
---
+++ src/pages/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import aboutBannerImg from "../images/about-banner.png";
+
+export default function About() {
+  return (
+    <section className="about-container">
+      <img
+        className="about-banner-img"
+        src={aboutBannerImg}
+        alt="A man is looking at the stars on top of a van in the night."
+      />
+      <div className="about-content">
+        <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
+        <p>
+          Our mission is to enliven your road trip with the perfect travel van
+          rental. Our vans are recertified before each trip to ensure your
+          travel plans can go off without a hitch. (Hitch costs extra 😉)
+        </p>
+        <p>
+          Our team is full of vanlife enthusiasts who know firsthand the magic
+          of touring the world on 4 wheels.
+        </p>
+        <div className="about-action-container">
+          <h2>
+            Your destination is waiting. <br />
+            Your van is ready.
+          </h2>
+          <Link className="link-button">Explore our vans</Link>
+        </div>
+      </div>
+    </section>
+  );
+}
