3bbed60db199b38256add64a67ada17b3a748525
Atabek <superatabekt@gmail.com>
2025-04-03 17:47:49 +0500

    Updated App.jsx, index.css and Added Footer.jsx, Header.jsx and Layout.jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -4,40 +4,21 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans";
 import VanDetail from "./pages/VanDetail";
+import Layout from "./components/Layout";
 
 import "./server/server";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <header>
-        <nav>
-          <Link className="logo" to="/">
-            #Vanlife
-          </Link>
-          <ul className="nav-list">
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                About
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/vans">
-                Vans
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
-      <footer>
-        <p>Ⓒ 2022 #VANLIFE</p>
-      </footer>
     </BrowserRouter>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <Link className="logo" to="/">
+          #Vanlife
+        </Link>
+        <ul className="nav-list">
+          <li className="nav-item">
+            <Link className="nav-link" to="/about">
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/vans">
+              Vans
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
