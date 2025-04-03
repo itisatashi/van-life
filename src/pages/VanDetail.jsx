86e03b1cae0d1d9f685615eb868510a79021c481
Atabek <superatabekt@gmail.com>
2025-04-03 17:18:44 +0500

    Updated index.css and VanDetail.jsx

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -1,5 +1,32 @@
 import React from "react";
+import { useParams, Link } from "react-router-dom";
 
 export default function VanDetail() {
-  return <h1>Hello, blabla!</h1>;
+  const params = useParams();
+  const [van, setVan] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch(`/api/vans/${params.id}`)
+      .then((res) => res.json())
+      .then((data) => setVan(data.vans));
+  }, [params.id]);
+
+  return (
+    <section className="van-detail-container">
+      {van ? (
+        <div className="van-detail">
+          <img src={van.imageUrl} alt={`An image of ${van.name}.`} />
+          <i className={`van-type van-type-${van.type}`}>{van.type}</i>
+          <h3 className="van-name">{van.name}</h3>
+          <p className="van-price">
+            ${van.price} <span>/day</span>
+          </p>
+          <p className="van-description">{van.description}</p>
+          <Link className="link-button">rent this van</Link>
+        </div>
+      ) : (
+        <h2 className="loading">Loading...</h2>
+      )}
+    </section>
+  );
 }
