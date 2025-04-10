2c55e6510d9e31e09002c5e3ff7b91124fb4e8cd
Atabek <superatabekt@gmail.com>
2025-04-11 00:40:31 +0500

    Use replace prop in Navigate for AuthRequiredLayout to prevent adding login route to history

--- src/components/AuthRequiredLayout.jsx
+++ src/components/AuthRequiredLayout.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, Navigate, replace } from "react-router-dom";
 
 export default function AuthRequiredLayout() {
   const loggedIn = localStorage.getItem("loggedin");
 
   if (!loggedIn) {
-    return <Navigate to="login" state={{ message: "You must first log in" }} />;
+    return (
+      <Navigate
+        to="login"
+        state={{ message: "You must first log in" }}
+        replace
+      />
+    );
   }
 
   return <Outlet />;
